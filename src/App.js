e343e05d1976a1750ca3951e8fa98c761e68cead
Anurag Gupta <anuragjigupta17@gmail.com>
2019-04-21 23:17:17 +0530

    Created and configured home page
    Completed the settting up of initial home page for the application.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home'
+import Welcome from './Welcome'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: 'Anurag'
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        { this.state.user && <Welcome user={this.state.user}/>}
+        <Home user={this.state.user}/>
       </div>
     );
   }
