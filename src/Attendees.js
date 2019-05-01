474d7832dc7672c0be3bab75cfad33b8a15744a2
Anurag Gupta <anuragjigupta17@gmail.com>
2019-05-01 21:26:09 +0530

    filter and search attendees
    filter and search attendees

--- src/Attendees.js
+++ src/Attendees.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import firebase from './Firebase';
 import AttendeesList from './AttendeesList';
+import { FaUndo, FaRandom } from 'react-icons/fa';
 
 class Attendees extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchQuery: '',
+      allAttendees: [],
       displayAttendees: []
     };
   }
@@ -33,12 +36,40 @@ class Attendees extends Component {
         }
 
         this.setState({
+            allAttendees: attendeesList,
             displayAttendees: attendeesList
         });
     });
   }
 
+  handleChange = e => {
+    const { name, value } = e.target
+    this.setState({ [name]: value });
+  }
+
+  resetQuery = () => {
+    this.setState({
+      displayAttendees: this.state.allAttendees,
+      searchQuery: ''
+    });
+  }
+
+  chooseRandom = () => {
+    const randomAttendee = Math.floor
+      (Math.random() * this.state.allAttendees.length);
+    this.resetQuery();
+    this.setState({
+      displayAttendees: [this.state.allAttendees[randomAttendee]]
+    })
+  }
+
   render() {
+    const dataFilter = item =>
+      item.attendeeName
+        .toLowerCase()
+        .match(this.state.searchQuery.toLowerCase()) && true;
+    const filterAttendees = this.state.displayAttendees.filter(dataFilter);
+
     return (
         <div className="container mt-4">
           <div className="row justify-content-center">
@@ -46,13 +77,43 @@ class Attendees extends Component {
               <h1 className="font-weight-light text-center">
                 Attendees
               </h1>
+              <div className="card bg-light mb-4">
+                <div className="card-body text-center">
+                  <div className="input-group input-group-lg">
+                    <input 
+                      type="text"
+                      name="searchQuery"
+                      value={this.state.searchQuery}
+                      placeholder="Search Attendee"
+                      className="form-control"
+                      onChange={this.handleChange}
+                    />
+                    <div className="input-group-append">
+                      <button 
+                        className="btn btn-sm btn-outline-secondary"
+                        title="Pcik a random attendee"
+                        onClick={() => this.chooseRandom()}
+                      >
+                        <FaRandom />
+                      </button>
+                      <button 
+                        className="btn btn-sm btn-outline-secondary"
+                        title="Reset search"
+                        onClick={() => this.resetQuery()}
+                      >
+                        <FaUndo />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <AttendeesList
             userID={this.props.userID}
             meetingID={this.props.meetingID}
             adminUser={this.props.adminUser}
-            attendees={this.state.displayAttendees}
+            attendees={filterAttendees}
           />
         </div>
     );
